52eac45c77476b81a9a22b94be7f3576ca3e8d98
Gabriel Arcanjo Campelo Fadoul <gabriel_arcanjo_campelo@hotmail.com>
2025-07-02 20:05:15 -0300

    Implementa chaveamento do database

--- app/config.py
+++ app/config.py
@@ -2,8 +2,11 @@ import os
 
 DB_URL_ENV = "TODO_CLI_DB_URL"
 
-def get_db_url() -> str:
-    return os.environ.get(DB_URL_ENV, "sqlite:///database.db")
+def get_db_url(test = False) -> str:
+    if test:
+        return os.environ.get(DB_URL_ENV, "sqlite://")
+    else:
+        return os.environ.get(DB_URL_ENV, "sqlite:///database.db")
 
 def set_db_url(url: str):
     os.environ[DB_URL_ENV] = url
--- app/database.py
+++ app/database.py
@@ -2,16 +2,18 @@
 from sqlmodel import create_engine, SQLModel, Session
 from app.config import get_db_url
 
+
 _engine = None
 
-def get_engine():
+def get_engine(test = False):
     global _engine
     if _engine is None:
-        _engine = create_engine(get_db_url(), connect_args={"check_same_thread": False})
+        _engine = create_engine(get_db_url(test) if test else get_db_url(),
+                                connect_args={"check_same_thread": False})
     return _engine
 
 def get_session():
     return Session(get_engine())
 
-def create_db_and_tables():
-    SQLModel.metadata.create_all(get_engine())
+def create_db_and_tables(test = False):
+    SQLModel.metadata.create_all(get_engine(test) if test else get_engine())
